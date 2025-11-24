c60edc890561b453393daad875b703b1b1754746
2022-491 <149299460+Ellyas-Prambudyas@users.noreply.github.com>
2025-11-24 13:25:49 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: '', // masukan api key dari database supabase
+    anonKey: '',
+  );
+
   runApp(const InventoryApp());
 }
