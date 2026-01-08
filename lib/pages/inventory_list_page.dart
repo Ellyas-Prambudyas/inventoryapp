12ce9b6e9252b6f54d873323894b98937b4ffcc9
2022-491 <149299460+Ellyas-Prambudyas@users.noreply.github.com>
2026-01-08 17:15:47 +0700

    Update inventory_list_page.dart

--- lib/pages/inventory_list_page.dart
+++ lib/pages/inventory_list_page.dart
@@ -34,7 +34,7 @@ class _InventoryListPageState extends State<InventoryListPage> {
         builder: (context, items, _) {
           if (items.isEmpty) {
             return const Center(
-              child: Text('Data barang kosong.'),
+              child: Text('Belum ada data barang.'),
             );
           }
           return ListView.builder(
